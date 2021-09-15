977dd9e3b64db57041344a4874a3fc1ab0d4d0ac
Self-Discipline <34881141+TheDetial@users.noreply.github.com>
2021-09-15 15:24:04 +0800

    Update __init__.py

--- Enet_lapa_camvid_pytorch/datasets/__init__.py
+++ Enet_lapa_camvid_pytorch/datasets/__init__.py
@@ -1,11 +1,7 @@
-from .pngpng_train import PngPngTrain
-from .pngpng_test import PngPng
 from .lapa_loader import LapaPngPng
 from .camvid_loader import CamvidPngPng
 
 __datasets__ = {
-    "pngpngtrain": PngPngTrain,
-    "pngpngtest": PngPng,
     "lapa": LapaPngPng,
     "camvid": CamvidPngPng
 }
